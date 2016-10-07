60a5589f88c5ca77986c36779b760c0e795433f2
seescode <seesyong@gmail.com>
2016-10-07 15:20:53 -0400

    save it

--- src/app/components/cart-list.ts
+++ src/app/components/cart-list.ts
@@ -4,17 +4,13 @@ import {CartItem} from './cart-item';
 @Component({
     selector: 'cart-list',
     template: `
-        Cart
         <ul *ngIf="cartList">
-            <cart-item
-                *ngFor="let cartItem of cartList"
+            <cart-item *ngFor="let cartItem of cartList"
                 [cartItem]="cartItem">
             </cart-item>
         </ul>
         <button class="pure-button pure-button-primary"
-            (click)="checkout.emit($event)">
-            Checkout
-         </button>
+            (click)="checkout())"> Checkout</button>
     `
 })
 export class CartList {
